db643b5686496d3a9b363c0e191af4ffcd231ccd
luonghieu911 <hieulx1st@gmail.com>
2023-04-25 10:50:00 +0700

    Khởi tạo dự án Laravel, add theme AdminLTE, Login

---
+++ qlsv/app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    //
+    public function index(){
+        return view('admin.users.login',['title'=>'Đăng nhập']);
+    }
+    public function login(Request $request){
+        //dd($request->input());
+        $this->validate($request,
+        [
+            'email'=>'required|email:filter',
+            'password'=> 'required',
+        ]);
+        if(Auth::attempt([
+            'email'=>$request->input('email'),
+            'password'=>$request->input('password')
+        ])){
+            return redirect()->route('admin');
+        }
+        return redirect()->back();
+    }
+}
---
+++ qlsv/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Admin\Users\HomeController;
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/user',[UserController::class,'index']);
+//Route::get('/user','App\Http\Controllers\UserController@index');
+Route::get('/login',[LoginController::class,'index']);
+Route::post('/admin/users/login',[LoginController::class,'login']);
+Route::get('/home',[HomeController::class,'index'])->name('admin');
